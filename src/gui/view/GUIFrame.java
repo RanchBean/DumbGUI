288057daa29b73bca1289333e6371c49ac0ee0ae
Ethan throndsen <Ethunderson1@gmail.com>
2015-11-09 12:34:24 -0700

    added JFrame to view package

--- src/gui/view/GUIFrame.java
+++ src/gui/view/GUIFrame.java
@@ -1,5 +1,15 @@
 package gui.view;
 
-public class GUIFrame {
+import javax.swing.JFrame;
+import gui.controller.GUIController;
 
+public class GUIFrame extends JFrame
+{
+
+	private GUIController baseController;
+	
+	public GUIFrame(GUIController baseController)
+	{
+		this.baseController = baseController; //Assign parameter to data member.
+	}
 }
